69bc4602e0cb9905d74332b3b80f5df033d8824b
zhangqi <1319419298@qq.com>
2019-08-27 22:46:14 +0800

    新增分页功能

--- THINK_SERVER/src/controller/news.js
+++ THINK_SERVER/src/controller/news.js
@@ -1,10 +1,23 @@
 // import {Common} from '../utils/common.js';
 const Base = require('./base.js');
+const pagination = require('think-pagination');
 module.exports = class extends Base {
   async indexAction() {
-    const newsList = await this.model('rubber_news').where({TYPE: 0}).select();
-    console.log('新闻' + newsList.length);
+    const option = {
+      desc: false, // show description
+      pageNum: 1,
+      url: '', // page url, when not set, it will auto generated
+      class: 'page', // pagenation extra class
+      text: {
+        next: 'Next',
+        prev: 'Prev',
+        total: 'count: __COUNT__ , pages: __PAGE__'
+      }
+    };
+    const newsList = await this.model('rubber_news').where({TYPE: 0}).page(this.get('page')).countSelect();
+    const html = pagination(newsList, {}, option);
     this.assign({newsList});
+    this.assign('pagination', html);
     return this.display();
   }
 };
